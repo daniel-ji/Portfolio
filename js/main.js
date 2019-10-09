e22729ac81af23e402ba4519a9c5f268a131b02a
Daniel Ji <Jidaniel1234@gmail.com>
2019-10-08 23:21:33 -0700

    addedpages

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 $('.nav, .home, .about, .resume, .project, .contact').hide();
 
 $(document).ready(function() {
+  let currentClass = 'home';
+  VANTA.NET({
+    el: `.${currentClass}`,
+    color: 0xffffff,
+    backgroundColor: 0x2755,
+    points: 12.00,
+    maxDistance: 22
+  })
+  //loading stuff
   loading_screen.finish();
   $('body > .inner').css('display', 'block');
   //particlesJS.load('particles-js', '../js/particlesjs-config.json');
-
-  var options = {
+  $('.home').show();
+  let options = {
     strings: ['Hi,^500 I\'m Daniel.'],
-    typeSpeed: 35
+    typeSpeed: 35,
   };
-
-  var options1 = {
+  const options1 = {
     strings: ['Welcome to my portfolio.'],
     typeSpeed: 35,
     startDelay: 2000
   };
+  const typed = new Typed('.home .title', options);
+  const typed1 = new Typed('.home .subtitle', options1);
+  const classList = [['home', 'about', 'resume', 'contact', 'project'], [0x002755, 0x205c80, 0x068eb8, 0x004463, 0x1E88E5], ['#002755', '#205c80', '#068eb8', '#004463', ' #1E88E5']];
 
-  var typed = new Typed('.title', options);
-  var typed = new Typed('.subtitle', options1);
-
-  $('.home').show();
+  //nav event listeners
 
   $('.menu').click(() => {
     if ($('.nav').hasClass('slideInDown')) {
-      $('.nav').removeClass('slideInDown');
-      $('.nav').addClass('slideOutUp');
-      $('.menu').attr("src", "../portfolio/css/images/menu.png");
+      navHide();
     } else {
       $('.nav').removeClass('slideOutUp');
       $('.nav').addClass('slideInDown');
@@ -33,6 +39,59 @@ $(document).ready(function() {
       $('.menu').attr("src", "../portfolio/css/images/close.png");
     }
   });
+
+  for (let i = 0; i < classList[0].length; i++) {
+    $(`#${classList[0][i]}`).click(() => {
+      console.log('asdf');
+      openOneCloseLast(classList[0][i], currentClass);
+      currentClass = classList[0][i];
+    });
+  }
+
+  //functions for nav
+  function openOneCloseLast(openClassName, closeClassName) {
+    if (openClassName != closeClassName) {
+      navHide();
+      $(`.${closeClassName}`).removeClass('slideInLeft');
+      $(`.${closeClassName}`).addClass('slideOutRight');
+      $(`.${openClassName}`).removeClass('slideOutRight');
+      $(`.${openClassName}`).addClass('slideInLeft');
+      $(`.${openClassName}`).show();
+
+      for (let i = 0; i < classList[0].length; i++) {
+        if (openClassName != classList[0][i] && closeClassName != classList[0][i]) {
+          $(`.${classList[0][i]}`).removeClass('slideInLeft');
+          $(`.${classList[0][i]}`).removeClass('slideOutRight');
+          $(`.${classList[0][i]}`).hide();
+        } else if (openClassName == classList[0][i]) {
+          $('body').css('background-color', classList[2][i]);
+          setTimeout(()=> {
+            VANTA.NET({
+              el: `.${openClassName}`,
+              color: 0xffffff,
+              backgroundColor: classList[1][i],
+              points: 12.00,
+              maxDistance: 22
+            });
+            $('canvas').hide();
+            $('canvas').fadeIn();
+          }, 1000);
+
+          //setTimeout(()=>{$('canvas').fadeIn();}, 2000);
+          setTimeout(() => {$('.nav').css('background-color', classList[2][i]);}, 500);
+        }
+      }
+    } else {
+      navHide();
+    }
+  }
+  function navHide() {
+    $('.nav').removeClass('slideInDown');
+    $('.nav').addClass('slideOutUp');
+    $('.menu').attr("src", "../portfolio/css/images/menu.png");
+  }
+
+
 
   /*
   const scene = new THREE.Scene();
